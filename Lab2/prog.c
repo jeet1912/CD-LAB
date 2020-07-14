d55719aaf928b531b0a9f30c1a9388d97cab1fcc
Jeet1912 <51080094+Jeet1912@users.noreply.github.com>
2020-07-14 10:48:34 +0530

    Add files via upload

---
+++ Lab2/prog.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+int prime(int n)
+{
+	int i;
+	for(i=0x2;i<sqrt(n);i++)
+	{
+		if(n%i==0 && i<=n)	
+		{
+			return 0;
+		}
+	}
+	return 1;
+}
+
+int main()
+{
+	int n,i;
+	int x[]={1,2,13,7};
+	printf("Enter a number");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+	if(prime(x[i]))
+		printf("A prime \n");
+	else
+		printf("not a prime \n");
+	}
+
+	return 0;
+}
